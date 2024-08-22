5e850b0b313b5358a9bad50a7999cc1d1cb5c0fe
Ramon Silva Freitas <ramonsilvaf@gmail.com>
2024-08-22 14:42:12 -0300

    mudança de pastas do aquivo processar.php para a raiz do sistema

--- processar.php
+++ processar.php
@@ -4,8 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../css/style.css">
-  <link rel="shortcut icon" href="../img/icone.png" type="image/x-icon">
+  <link rel="stylesheet" href="public/css/style.css">
+  <link rel="shortcut icon" href="public/img/icone.png" type="image/x-icon">
   <title>Resultados - Vendas</title>
 </head>
 
